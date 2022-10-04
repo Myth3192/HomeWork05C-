b5399667b4a0c5a2f13406547431374fb693dfc6
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-10-04 21:28:42 +0300

    Task34 added

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] array = new int [4];
+Random rand = new Random();
+int EvenAmount = 0;
+for (int i=0; i <array.Length; i++)
+{
+    array[i]=rand.Next(100,999);
+    Console.WriteLine(array[i]);
+}
+for (int i=0; i <array.Length; i++)
+{
+    if(array[i]%2==0)
+    {
+        EvenAmount++;
+    }
+}
+Console.WriteLine($"Количество четных чисел :{EvenAmount}");
