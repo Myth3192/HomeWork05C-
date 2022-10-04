c5d3bd3783c3b16c09dbb62cc28601d533a949ca
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-10-04 21:37:25 +0300

    Task36 added

---
+++ Task36/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = new int [4];
+Random rand = new Random();
+int elemsumm = 0;
+for (int i=0; i <array.Length; i++)
+{
+    array[i]=rand.Next(-100,100);
+    Console.WriteLine(array[i]);
+}
+for (int i=0; i <array.Length; i++)
+{
+    if (i%2==0)
+    {
+        elemsumm = elemsumm + array[i];
+    }
+}
+Console.WriteLine($"Сумма нечетных элемиентов : {elemsumm}");
