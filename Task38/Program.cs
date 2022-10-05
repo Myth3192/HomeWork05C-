115841f7649c4ae22ec6cf2f72d775577bfb66da
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-10-05 12:23:14 +0300

    Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,34 @@
+﻿int [] array = new int [5];
+Random rand = new Random();
+int elemsumm = 0;
+int maxnumber = 0;
+int minnumber = 0;
+for (int i=0; i <array.Length; i++)
+{
+    array[i]=rand.Next(-100,100);
+    Console.WriteLine(array[i]);
+}
+
+
+for (int i = 0; i < array.Length; i++ )
+{    
+    if (maxnumber<array[i])
+    {
+        maxnumber = array[i];
+    }
+
+}
+Console.WriteLine($"Max number is: {maxnumber}");
+
+
+for (int i = 0; i < array.Length; i++ )
+{    
+    if (minnumber>array[i])
+    {
+        minnumber = array[i];
+    }
+
+}
+Console.WriteLine($"Min number is: {minnumber}");
+
+Console.WriteLine($"Разница между максимальным элементом массива и минимальный составляет: {maxnumber-minnumber}");
